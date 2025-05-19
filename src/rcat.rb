9f30e767b35c4b9d6e246347ecb1b6983d6a7001
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-05-19 12:07:16 +0900

    コマンドラインで指定した全てのファイルの各行を 1 行ずつ画面に表示

--- src/rcat.rb
+++ src/rcat.rb
@@ -1,8 +1,9 @@
-# コマンドラインで指定したファイルの各行を 1 行ずつ画面に表示
-PATH = ARGV[0]
-
-File.open PATH, "r" do |file|
-  while line = file.gets&.chomp
-    puts line
+# コマンドラインで指定した全てのファイルの各行を 1 行ずつ画面に表示
+# ruby rcat.rb a.txt b.txt c.rb
+ARGV.each do |path|
+  File.open path, "r" do |file|
+    while line = file.gets&.chomp
+      puts line
+    end
   end
 end

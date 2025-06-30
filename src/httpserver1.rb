279c6af8995dab6146e0f927ee2b37ce2be1d3a7
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-06-30 12:39:02 +0900

    HTTP/1.0 に対応したサーバの実装

--- src/httpserver1.rb
+++ src/httpserver1.rb
@@ -1,22 +1,53 @@
 #! /usr/bin/ruby
 require "socket"
 
+# 前回
 # echo server
 # 1. TCP のポート 80 で接続を待ち受ける
 # 2. while の繰り返しで、行を読み込む
 #  2.1 読み込んだ行をソケットに送り返す
 
+# 今回
+# 簡易的な Web サーバを実装しよう
+
 s0 = TCPServer.open(80)
 
 while socket = s0.accept
   Thread.new do
-    while line = socket.gets&.chomp
-      puts line
-      socket.puts line
-      break if line.empty?
+    line = socket.gets&.chomp
+    _method, path, httpver = line&.split
+
+    case path
+    when "/93"
+      socket.puts "#{httpver} 301 Moved Permanently"
+      socket.puts "Location: https://www.kyusan-u.ac.jp/"
+      socket.puts
+      socket.puts ""
+
+    when "/hello"
+      socket.puts "#{httpver} 200 OK"
+      socket.puts
+      socket.puts "Hello, World!"
+
+    else
+      file_path = ".#{path}"
+      if File.exist? file_path
+        socket.puts "#{httpver} 200 OK"
+        socket.puts "Content-Type: text/plain; charset=UTF-8"
+        socket.puts
+        File.open file_path, "r" do |file|
+          while data = file.gets&.chomp
+            socket.puts data
+          end
+        end
+
+      else
+        socket.puts "#{httpver} 404 Not Found"
+        socket.puts
+      end
     end
+
     socket.close
-    sleep 5
   end
 end
 s0.close

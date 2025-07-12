f5208606201dcf28c438bba3a098bee3234e8f8f
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-07-12 17:12:04 +0000

    リファクタリング

--- src_report/client.rb
+++ src_report/client.rb
@@ -4,7 +4,11 @@ require "socket"
 # エントリーポイント
 def main
   socket = TCPSocket.open "localhost", 80
-  socket.puts "Hello, Server!\r\nHave a nice day!\r\n\r\n"
+
+  socket.puts "Hello, Server!"
+  socket.puts "Have a nice day!"
+  socket.puts ""
+
   while line = socket.gets&.chomp
     puts line
     break if line.empty?
